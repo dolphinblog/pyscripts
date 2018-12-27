c37c1a02124ff0953723ab64937d48f97886736e
vinsonw <me@vinsonwei.com>
2018-12-28 00:20:39 +0800

    add send_eamil_using_flask.py

---
+++ send_email_using_flask.py
@@ -0,0 +1,12 @@
+# 直接命令行输入
+export MAIL_USERNAME=网易邮箱
+export MAIL_PASSWORD=授权码
+
+# Python shell中输入
+from flask_mail import Message
+from hello import mail
+msg = Message('邮件标题', sender='网易邮箱', recipients=['收件邮箱'])
+msg.body = '邮件内容'
+msg.html = '邮件正文，支持html'
+with app.app_context():
+    mail.send(msg)
